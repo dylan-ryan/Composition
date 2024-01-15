63c9e9b5d04716f15772ba2d9b93fbd65030338f
Dylan <dylanryan431@gmail.com>
2024-01-15 14:11:22 -0400

    Initial commit

---
+++ Composition/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Composition");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
